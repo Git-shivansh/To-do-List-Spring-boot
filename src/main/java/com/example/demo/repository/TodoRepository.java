802cea8cedb7527f919f8dca4dc01e0f4219b816
Shivansh Tiwari <stshivansh354@gmail.com>
2026-05-30 14:27:47 +0530

    api's working

--- src/main/java/com/example/demo/repository/TodoRepository.java
+++ src/main/java/com/example/demo/repository/TodoRepository.java
@@ -2,7 +2,8 @@ package com.example.demo.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import com.example.demo.modals.*;
+
+import com.example.demo.modals.Todo;
 @Repository
 public interface TodoRepository extends JpaRepository<Todo, Long> {
 }
